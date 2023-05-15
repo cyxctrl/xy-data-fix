953fe05ea1e8fa8ae17766bb53fcf40e2e22dd92
cyxctrl <>
2023-05-15 14:06:01 +0800

    20230515_confirm.sh

---
+++ 20230515_confirm.sh
@@ -0,0 +1,9 @@
+curl http://127.0.0.1:8080/order/transaction/confirm -X POST -H "Content-type:application/json" -d '{"orderId":"1000252159948","buyerId":0}';
+curl http://127.0.0.1:8080/order/transaction/confirm -X POST -H "Content-type:application/json" -d '{"orderId":"1000252159947","buyerId":0}';
+curl http://127.0.0.1:8080/order/transaction/confirm -X POST -H "Content-type:application/json" -d '{"orderId":"1000252160042","buyerId":0}';
+curl http://127.0.0.1:8080/order/transaction/confirm -X POST -H "Content-type:application/json" -d '{"orderId":"1000252159777","buyerId":0}';
+curl http://127.0.0.1:8080/order/transaction/confirm -X POST -H "Content-type:application/json" -d '{"orderId":"1000252160609","buyerId":0}';
+curl http://127.0.0.1:8080/order/transaction/confirm -X POST -H "Content-type:application/json" -d '{"orderId":"1000252081302","buyerId":0}';
+curl http://127.0.0.1:8080/order/transaction/confirm -X POST -H "Content-type:application/json" -d '{"orderId":"1000252151519","buyerId":0}';
+curl http://127.0.0.1:8080/order/transaction/confirm -X POST -H "Content-type:application/json" -d '{"orderId":"1000252000064","buyerId":0}';
+curl http://127.0.0.1:8080/order/transaction/confirm -X POST -H "Content-type:application/json" -d '{"orderId":"1000252092128","buyerId":0}'
